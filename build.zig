b69cfe542a03ef7f912ef0aaa498d7cc2fce7c98
mejobloggs <mejobloggs@gmail.com>
2023-06-08 22:39:51 +1200

    init

---
+++ build.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = std.zig.CrossTarget{
+        .cpu_arch = std.Target.Cpu.Arch.riscv32,
+        .os_tag = std.Target.Os.Tag.freestanding,
+        .abi = std.Target.Abi.none,
+        .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
+        .cpu_features_add = std.Target.riscv.featureSet(&[_]std.Target.riscv.Feature{ .c, .m, .a }),
+    };
+
+    const optimize = std.builtin.OptimizeMode.ReleaseSmall;
+
+    const exe = b.addExecutable(.{
+        .name = "weact-ch582f",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const link_file_path = comptime root() ++ "link.ld";
+    exe.setLinkerScriptPath(std.build.FileSource{ .path = link_file_path });
+
+    b.installArtifact(exe);
+}
+
+fn root() []const u8 {
+    return comptime (std.fs.path.dirname(@src().file) orelse ".") ++ "/";
+}
---
+++ src/main.zig
@@ -0,0 +1,15 @@
+pub fn main() !void {
+    //This is a barebones attempt to turn the LED on, based on WeAct blinky example provided here:
+    //https://github.com/WeActStudio/WeActStudio.WCH-BLE-Core/blob/master/Examples/CH582/template/src/Main.c
+
+    //Copied address from: https://github.com/WeActStudio/WeActStudio.WCH-BLE-Core/blob/ade0ae478505ff131c9b7ecd9d6a2aaf7b009e51/Examples/CH582/template/StdPeriphDriver/inc/CH583SFR.h#L516
+    const port_a_dir_location = @intToPtr(*volatile u32, 0x400010A0);
+
+    //Copied address from: https://github.com/WeActStudio/WeActStudio.WCH-BLE-Core/blob/ade0ae478505ff131c9b7ecd9d6a2aaf7b009e51/Examples/CH582/template/StdPeriphDriver/inc/CH583SFR.h#L522
+    const port_a_out_location = @intToPtr(*volatile u32, 0x400010A8);
+
+    //0x00000100 = Pin8, copied from: https://github.com/WeActStudio/WeActStudio.WCH-BLE-Core/blob/ade0ae478505ff131c9b7ecd9d6a2aaf7b009e51/Examples/CH582/template/StdPeriphDriver/inc/CH58x_gpio.h#L29
+
+    port_a_dir_location.* = (0x00000100);
+    port_a_out_location.* = (0x00000100);
+}
